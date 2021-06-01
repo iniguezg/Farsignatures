718371fbad01832737b6b446c62a04ebd2be04ec
iniguezg <iniguez.gerardo@gmail.com>
2021-06-01 12:44:59 +0300

    triton scripts

--- script_getData_triton.sh
+++ script_getData_triton.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=egonet_fits_text
-#SBATCH --output=egonet_fits_text_%a.out
-#SBATCH --array=1-409
+#SBATCH --job-name=egonet_fits_call
+#SBATCH --output=egonet_fits_call_%a.out
+#SBATCH --array=1-895
 #SBATCH --time=05-00
 #SBATCH --mem-per-cpu=2G
 
@@ -18,5 +18,5 @@
 #analysis 5
 #find . ! -name . -prune -type f -name 'egonet_props_text_*' > filenames_text.txt
 n=$SLURM_ARRAY_TASK_ID
-filename=`sed -n "${n} p" filenames_text.txt`
+filename=`sed -n "${n} p" filenames_call.txt`
 srun python script_getData.py ${filename}
