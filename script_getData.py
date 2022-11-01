9cb3bbf602df5eafe60c1fecf66f35b6d819bfe7
iniguezg <iniguez.gerardo@gmail.com>
2022-11-01 18:30:55 +0100

    triton scripts

--- script_getData.py
+++ script_getData.py
@@ -19,36 +19,36 @@ if __name__ == "__main__":
 	#input arguments
 	loadflag = 'n' #load flag ( 'y', 'n' )
 
-	#SMALL DATASETS
-	root_data = expanduser('~') + '/prg/xocial/datasets/temporal_networks/' #root location of data/code
-	root_code = expanduser('~') + '/prg/xocial/Farsignatures/'
-	saveloc = root_code+'files/data/' #location of output files
-	datasets = [
-				 ('Copenhagen_nets', 'CNS_bt_symmetric.evt'),
-				 ('Copenhagen_nets', 'CNS_calls.evt'),
-				 ('Copenhagen_nets', 'CNS_sms.evt'),
-				 ('greedy_walk_nets', 'email.evt'),
-				 ('greedy_walk_nets', 'eml2.evt'),
-				 ('greedy_walk_nets', 'fb.evt'),
-				 ('greedy_walk_nets', 'forum.evt'),
-				 ('greedy_walk_nets', 'messages.evt'),
-				 ('greedy_walk_nets', 'pok.evt'),
-				 ('MPC_UEu_net', 'MPC_UEu.evt'),
-				 ('SMS_net', 'MPC_Wu_SD01.evt'),
-				 ('SMS_net', 'MPC_Wu_SD02.evt'),
-				 ('SMS_net', 'MPC_Wu_SD03.evt'),
-				 ('sex_contacts_net', 'sexcontact_events.evt'),
-				 ('QA_nets', 'QA_askubuntu.evt'),
-				 ('QA_nets', 'QA_mathoverflow.evt'),
-				 ('QA_nets', 'QA_superuser.evt'),
-				 ('SNAP', 'email_Eu_core.evt'),
-				 ('SNAP', 'CollegeMsg.evt'),
-				 ('Enron', 'Enron.evt')
-				]
+	# #SMALL DATASETS
+	# root_data = expanduser('~') + '/prg/xocial/datasets/temporal_networks/' #root location of data/code
+	# root_code = expanduser('~') + '/prg/xocial/Farsignatures/'
+	# saveloc = root_code+'files/data/' #location of output files
+	# datasets = [
+	# 			 ('Copenhagen_nets', 'CNS_bt_symmetric.evt'),
+	# 			 ('Copenhagen_nets', 'CNS_calls.evt'),
+	# 			 ('Copenhagen_nets', 'CNS_sms.evt'),
+	# 			 ('greedy_walk_nets', 'email.evt'),
+	# 			 ('greedy_walk_nets', 'eml2.evt'),
+	# 			 ('greedy_walk_nets', 'fb.evt'),
+	# 			 ('greedy_walk_nets', 'forum.evt'),
+	# 			 ('greedy_walk_nets', 'messages.evt'),
+	# 			 ('greedy_walk_nets', 'pok.evt'),
+	# 			 ('MPC_UEu_net', 'MPC_UEu.evt'),
+	# 			 ('SMS_net', 'MPC_Wu_SD01.evt'),
+	# 			 ('SMS_net', 'MPC_Wu_SD02.evt'),
+	# 			 ('SMS_net', 'MPC_Wu_SD03.evt'),
+	# 			 ('sex_contacts_net', 'sexcontact_events.evt'),
+	# 			 ('QA_nets', 'QA_askubuntu.evt'),
+	# 			 ('QA_nets', 'QA_mathoverflow.evt'),
+	# 			 ('QA_nets', 'QA_superuser.evt'),
+	# 			 ('SNAP', 'email_Eu_core.evt'),
+	# 			 ('SNAP', 'CollegeMsg.evt'),
+	# 			 ('Enron', 'Enron.evt')
+	# 			]
 
-	# #LARGE DATASETS
-	# root_data = '/m/cs/scratch/networks-mobile/heydars1/set5_divided_to_small_files_for_gerardo_29_march_2021/'
-	# saveloc = '/m/cs/scratch/networks/inigueg1/prg/xocial/Farsignatures/files/data/'
+	#LARGE DATASETS
+	root_data = '/m/cs/scratch/networks-mobile/heydars1/set5_divided_to_small_files_for_gerardo_29_march_2021/'
+	saveloc = '/m/cs/scratch/networks/inigueg1/prg/xocial/Farsignatures/files/data/'
 	# datasets = [ ( 'divided_to_roughly_40_mb_files_30_march', 'call' ), ( 'divided_to_roughly_40_mb_files_30_march', 'text' ) ]
 
 
